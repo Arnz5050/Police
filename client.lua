4c637edfb906e7d63bbd19dd0c8dcdf8118e6d63
Arnz5050 <68950015+Arnz5050@users.noreply.github.com>
2023-06-04 17:45:53 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -4,44 +4,45 @@ local LastZone, CurrentAction, CurrentActionMsg
 
 -- Vehicle Spawn Menu
 function OpenSpawnerMenu()
-    elements = 
+    elements =
     {
         {unselectable = true, icon = "fas fa-car", title = "Police Vehicles"}
     }
-
-    ESX.OpenContext("right", elements)
-    if ESX.Game.IsSpawnPointClear(this_Spawner.Loc, 5.0)
-    then
-        for i = 1, #Config.Vehicles, 1 do
-            elements[#elements+1] =
-            {
-                title = Config.Vehicles[i].label,
-                model = Config.Vehicles[i].model,
-            }
-        end
-        ESX.OpenContext("right", elements, function(menu,element)
-            CurrentActionData = {}
-            Properties = 
-            {
-                plate = 'NOTACOP',
-                modTurbo = true
-            }
-            SpawnVehicle()
-            end, function(menu)
-        end)
-    else
-        ESX.CloseContext()
-        CurrentAction = nil
-        CurrentActionData = {}
-        ESX.ShowNotification(_U('spawnpoint_blocked'))
+    for i = 1, #Config.Vehicles, 1 do
+    elements[#elements+1] =
+    {
+        title = Config.Vehicles[i].label,
+        model = Config.Vehicles[i].model
+    }
     end
+
+    ESX.OpenContext("right", elements, function(menu, data)
+
+        print(json.encode(data)) -- check the console log for this
+        result1 = json.encode(data.model)
+        print(result1)
+        fresult = data.model
+        print(fresult)
+        vehHash = fresult
+
+        if ESX.Game.IsSpawnPointClear(Config.Zones.VehicleSpawner1.Loc, 5.0) then
+            SpawnVehicle(data.model) -- check the exact key for model in data obj
+            ESX.ShowNotification(TranslateCap('vehicle_spawned'), "success")
+            ESX.CloseContext()
+        else
+            ESX.ShowNotification(TranslateCap('spawnpoint_blocked'))
+        end
+    end)
 end
 
-function SpawnVehicle()
-    ESX.Game.SpawnVehicle(RequestModel(elements.model), Config.Zones.VehicleSpawner1.Loc, Config.Zones.VehicleSpawner1.Heading, Properties, function(vehicle)
-    TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
-end)
+
+function SpawnVehicle(string) -- declare parameter for the model string
+    ESX.Game.SpawnVehicle(fresult, Config.Zones.VehicleSpawner1.Loc, Config.Zones.VehicleSpawner1.Heading, function(vehicle)
+        TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
+    end)
 end
+
+
 
 -- Vehicle Return Menu
 function OpenReturnMenu()
@@ -69,7 +70,7 @@ function isVehicleListed(Model)
 end
 
 -- Entered Marker
-AddEventHandler('test:hasEnteredMarker', function(zone)
+AddEventHandler('hasEnteredMarker', function(zone)
 if zone == 'spawner_point' then
     CurrentAction = 'spawner_point'
     CurrentActionMsg = _U('press_to_enter')
@@ -82,63 +83,70 @@ end
 end)
 
 -- Exited Marker
-AddEventHandler('test:hasExitedMarker', function()
+AddEventHandler('hasExitedMarker', function()
     ESX.CloseContext()
+    CurrentAction = nil
 end)
 
 -- Resource Stop
 AddEventHandler('onResourceStop', function(resource)
     ESX.CloseContext()
+    CurrentAction = nil
 end)
 
 -- Enter / Exit marker events & Draw Markers
 CreateThread(function()
 while true do
-    Wait(0)
-    local playerCoords = GetEntityCoords(PlayerPedId())
-    local isInMarker, letSleep, currentZone = false, true, nil
+    
+    if ESX.PlayerData.job and ESX.PlayerData.job.name == 'police' then
+        Wait(0)
+        local playerCoords = GetEntityCoords(PlayerPedId())
+        local isInMarker, letSleep, currentZone = false, true, nil
 
-    for k,v in pairs(Config.Zones) do
-        local distance = #(playerCoords - v.Pos)
-        local distance2 = #(playerCoords - v.Del)
+        for k,v in pairs(Config.Zones) do
+            local distance = #(playerCoords - v.Pos)
+            local distance2 = #(playerCoords - v.Del)
 
-        if distance < Config.DrawDistance then
-            letSleep = false
+            if distance < Config.DrawDistance then
+                letSleep = false
 
-            if Config.MenuMarker.Type ~= -1 then
-                DrawMarker(Config.MenuMarker.Type, v.Pos, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.MenuMarker.x, Config.MenuMarker.y, Config.MenuMarker.z, Config.MenuMarker.r, Config.MenuMarker.g, Config.MenuMarker.b, 100, false, true, 2, false, false, false, false)
+                if Config.MenuMarker.Type ~= -1 then
+                    DrawMarker(Config.MenuMarker.Type, v.Pos, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.MenuMarker.x, Config.MenuMarker.y, Config.MenuMarker.z, Config.MenuMarker.r, Config.MenuMarker.g, Config.MenuMarker.b, 100, false, true, 2, false, false, false, false)
+                end
+
+                if distance < Config.MenuMarker.x then
+                    isInMarker, this_Spawner, currentZone = true, v, 'spawner_point'
+                end
             end
 
-            if distance < Config.MenuMarker.x then
-                isInMarker, this_Spawner, currentZone = true, v, 'spawner_point'
+            if distance2 < Config.DrawDistance then
+                letSleep = false
+
+                if Config.DelMarker.Type ~= -1 then
+                    DrawMarker(Config.DelMarker.Type, v.Del, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.DelMarker.x, Config.DelMarker.y, Config.DelMarker.z, Config.DelMarker.r, Config.DelMarker.g, Config.DelMarker.b, 100, false, true, 2, false, false, false, false)
+                end
+
+                if distance2 < Config.DelMarker.x then
+                    isInMarker, this_Spawner, currentZone = true, v, 'deleter_point'
+                end
             end
         end
 
-        if distance2 < Config.DrawDistance then
-            letSleep = false
-
-            if Config.DelMarker.Type ~= -1 then
-                DrawMarker(Config.DelMarker.Type, v.Del, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.DelMarker.x, Config.DelMarker.y, Config.DelMarker.z, Config.DelMarker.r, Config.DelMarker.g, Config.DelMarker.b, 100, false, true, 2, false, false, false, false)
-            end
-
-            if distance2 < Config.DelMarker.x then
-                isInMarker, this_Spawner, currentZone = true, v, 'deleter_point'
-            end
+        if (isInMarker and not HasAlreadyEnteredMarker) or (isInMarker and LastZone ~= currentZone) then
+            HasAlreadyEnteredMarker, LastZone = true, currentZone
+            TriggerEvent('hasEnteredMarker', currentZone)
         end
-    end
 
-    if (isInMarker and not HasAlreadyEnteredMarker) or (isInMarker and LastZone ~= currentZone) then
-        HasAlreadyEnteredMarker, LastZone = true, currentZone
-        TriggerEvent('test:hasEnteredMarker', currentZone)
-    end
+        if not isInMarker and HasAlreadyEnteredMarker then
+            HasAlreadyEnteredMarker = false
+            TriggerEvent('hasExitedMarker', LastZone)
+        end
 
-    if not isInMarker and HasAlreadyEnteredMarker then
-        HasAlreadyEnteredMarker = false
-        TriggerEvent('test:hasExitedMarker', LastZone)
-    end
-
-    if letSleep then
-        Wait(500)
+        if letSleep then
+            Wait(500)
+        end
+    else
+        Wait(sleep)
     end
 end
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,13 +9,6 @@ shared_script '@es_extended/imports.lua'
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/*.lua',
-	'config.lua',
-	'client.lua'
-}
-
-server_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
-	'server.lua'
+	'client.lua',
+	'config.lua'
 }
