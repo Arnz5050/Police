c5ec3cb6a0207d12e5d6e4bb423a6b5b9c03b18a
Arnz5050 <68950015+Arnz5050@users.noreply.github.com>
2023-06-04 17:47:13 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,8 +5,6 @@ Config.DrawDistance = 50 -- Marker Draw Distance.
 Config.MenuMarker = {Type = 1, r = 0, g = 255, b = 0, x = 1.5, y = 1.5, z = 1.0} -- Enter Location Marker Settings.
 Config.DelMarker = {Type = 1, r = 255, g = 0, b = 0, x = 5.0, y = 5.0, z = 1.0} -- Delete Location Marker Settings.
 
-Config.UseBlips = false -- true = Use Vehicle Spawner Blips.
-
 Config.Zones = {
 	VehicleSpawner1 = { 
 		Pos = vector3(424, -1012, 28), -- Enter Marker
